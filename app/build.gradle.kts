c3b598368faee95ff3a35cfe7f39a261bb8dcca2
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 13:57:03 +0300

    Release v0.9 contrast recognition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 8
-        versionName = "0.8-shapes"
+        versionName = "0.9-contrast"
     }
 
     compileOptions {
