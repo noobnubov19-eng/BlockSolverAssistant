76517e76afa6ae61947017d2cf887277bc6087be
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 15:15:21 +0300

    Release v1.2 Adaptive Combo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.blocksolver"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1-stable"
+        versionCode = 12
+        versionName = "1.2-adaptive-combo"
     }
 
     compileOptions {
