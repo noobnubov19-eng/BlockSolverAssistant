42c9ef6acea4e631d403730242e382a00ef53b0a
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 17:39:09 +0300

    Release v1.8 Vision Lock

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 18
-        versionName = "1.8-lock"
+        versionName = "1.8-vision-lock"
     }
 
     compileOptions {
