12ea49e3c8d8e9146e27cf9e0841e59845f5b54d
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 14:17:53 +0300

    Release v1.1 Stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.blocksolver"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0-survival"
+        versionCode = 11
+        versionName = "1.1-stable"
     }
 
     compileOptions {
