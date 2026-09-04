e6bbcb11cb6bd6127bba13bef628c99e90947a96
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 23:41:46 +0300

    Release v2.3 Combo Math

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.blocksolver"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.2-combo-king"
+        versionCode = 23
+        versionName = "2.3-combo-math"
     }
 
     compileOptions {
