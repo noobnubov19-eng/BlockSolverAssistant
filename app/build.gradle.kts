f085f5184db09c167b3ef33ed60439d900ab2f96
noobnubov19-eng <noob.nubov.19@gmail.com>
2026-09-04 14:05:00 +0300

    Release v1.0 Survival

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 8
-        versionName = "0.9-contrast"
+        versionName = "1.0-survival"
     }
 
     compileOptions {
